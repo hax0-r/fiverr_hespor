59c47b003af03b916bf2cb9d67e271e3b94f07df
Muhammad Talha <talha18513@gmail.com>
2025-06-06 19:15:01 +0500

    update

--- docs/javascript/global.js
+++ docs/javascript/global.js
@@ -6,7 +6,7 @@ const bottomToTopScroll = document.getElementById("bottomToTopScroll");
 
 bottomToTopScroll.innerHTML = `
 <div
-    class="bottomToTop fadeIn w-10 cursor-pointer z-40 bg-[#012929] h-10 fixed bottom-5 right-5 hover:opacity-80 transition-all duration-500 hidden text-zinc-100 flex items-center justify-center rounded-lg "><i class="fa-solid fa-angle-up"></i>
+    class="bottomToTop fadeIn w-10 cursor-pointer z-40 bg-[#012929] h-10 fixed bottom-5 right-5 hover:opacity-80 transition-all duration-500 hidden text-zinc-100 flex border border-zinc-300 items-center justify-center rounded-lg "><i class="fa-solid fa-angle-up"></i>
 </div>`
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -24,6 +24,16 @@ document.addEventListener("DOMContentLoaded", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
 });
+
+ // JavaScript to play/pause on hover
+  document.querySelectorAll('.group').forEach((card) => {
+    const video = card.querySelector('video');
+    card.addEventListener('mouseenter', () => video.play());
+    card.addEventListener('mouseleave', () => {
+      video.pause();
+      video.currentTime = 0; // Reset to start
+    });
+  });
 
 AOS.init({
   once: true,
